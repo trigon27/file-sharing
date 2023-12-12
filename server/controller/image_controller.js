8dd0a4c4718c436c91e38e2df40f71b314b6d55f
trigon27 <ashrafshaikh0778866@gmail.com>
2023-12-12 17:41:06 +0530

    add env

--- server/controller/image_controller.js
+++ server/controller/image_controller.js
@@ -1,16 +1,19 @@
 import File from "../models/file.js";
+import dotenv from 'dotenv'
+dotenv.config();
 
 export const uploadImage = async (request, response) => {
   const fileObj = {
     path: request.file.path,
     name: request.file.originalname,
   };
+  const BASE_url=process.env.BASE_URL;
 
   try {
     console.log("Attempting to create file:", fileObj);
     const file = await File.create(fileObj);
     console.log("File created successfully:", file);
-    response.status(200).json({ path:`http://localhost:8000/file/${file._id}` });
+    response.status(200).json({path:`${BASE_url}/file/${file._id}`} );
   } catch (error) {
     console.error("Error in image_controller:", error.message);
     response.status(500).json({ error: "Internal Server Error" });
