6905673809b2881d7731674fca6ae73a18451020
trigon27 <ashrafshaikh0778866@gmail.com>
2023-12-17 21:13:37 +0530

    add Databae url to env

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const DBconnection=async()=>{
-    const MONO_URL=`mongodb://trigon27:7718985075@ac-xspnawe-shard-00-00.lcsvj7q.mongodb.net:27017,ac-xspnawe-shard-00-01.lcsvj7q.mongodb.net:27017,ac-xspnawe-shard-00-02.lcsvj7q.mongodb.net:27017/?ssl=true&replicaSet=atlas-k8raxw-shard-0&authSource=admin&retryWrites=true&w=majority`
+    const MONO_URL=process.env.DATABASE
     try{
        await mongoose.connect(MONO_URL);
        console.log("mongoDB connected successfully ")
